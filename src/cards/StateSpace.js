823fe24e6e4340f2349d244264429c6eed1a9ce7
Elena Williams <elena.sivanjali.williams@gmail.com>
2022-07-25 00:43:21 -0700

    canvas changes based on common interval

--- src/cards/StateSpace.js
+++ src/cards/StateSpace.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
 export default function StateSpace(props) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'hsl('+ (props.time * 2) +',50%,70%)';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  }, [props.time])
+
+  const grid = props.grid;
+
   return (
-    <div id="StateSpace">{props.grid.limits.x.lower}, {props.grid.origin.y}</div>
+    <div id="StateSpace">
+      <canvas ref={canvasRef} width={grid.canvasSize.x} height={grid.canvasSize.y}></canvas>
+    </div>
   )
 }
--- src/simulations/Pendulum.js
+++ src/simulations/Pendulum.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from '../NavBar.js';
 import { STAGE_DIMENSIONS } from '../constants.js';
 import {Energy, Options, Readout, StateSpace, Visualization} from '../cards/index.js';
 import {PendulumData as data} from './simulation_data/PendulumData.js';
 
+
 export default function Pendulum(props) {
+
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {setInterval(() => {
+    setTime(prevTime => prevTime + 1);
+  }, 50);}, []);
 
   const style = {
     width: STAGE_DIMENSIONS.width + 'px',
@@ -13,13 +20,14 @@ export default function Pendulum(props) {
   };
 
 
+
   return (
     <>
         <NavBar switchTheme={props.switchTheme} theme={props.theme} title={data.title}/>
         <div id="stage-wrapper">
           <div className="Stage" style={style}>
             <Visualization/>
-            <StateSpace grid={data.grid}/>
+            <StateSpace grid={data.grid} time={time}/>
             <Energy/>
             <Options/>
             <Readout/>
