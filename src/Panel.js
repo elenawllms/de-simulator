d36e9a9d55b10d34c036eac1755fabf948b43e62
Elena Williams <elena.sivanjali.williams@gmail.com>
2022-08-04 10:53:55 -0700

    tabs!

--- src/Panel.js
+++ src/Panel.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {Energy, Options, Readout, StateSpace} from './cards/index.js';
 
+
+
+
+
 export default function Panel(props) {
+
+  const [activeTab, setActiveTab] = useState('State Space');
+
+  const tabNames = [
+    'State Space', 'Options', 'Energy Diagram', 'Time Series'
+  ]  
+
+  const tabs = [
+    <StateSpace key='State Space' label='State Space' grid={props.data.grid} state={props.state}/>,
+    <Options key='Options' label='Options'/>,
+    <Energy key='Energy Diagram' label='Energy Diagram'/>,
+    <Readout key='Time Series' label='Time Series'/>
+  ]
+
   return (
     <div id="Panel">
-        <StateSpace grid={props.data.grid} state={props.state}/>
+        <div className='tabs'>
+          <ol className='tab-list'>
+            {tabNames.map((label) => {
+              return <li key={label} 
+                          className={activeTab === label ? 'tab-list-active' : 'tab-list-inactive'}
+                          onClick={() => {setActiveTab(label)}}>
+                            {label}
+                      </li>})}
+          </ol>
+        </div>
+        <div className='panel-content'>
+          {tabs.map((tab) => {
+            return (tab.props.label !== activeTab) ? undefined : tab
+          })}
+        </div>
     </div>
   )
 }
