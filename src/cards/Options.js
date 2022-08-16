68722054cab0d8c39fad5d75b81c15a31a84f21b
Elena Williams <elena.sivanjali.williams@gmail.com>
2022-08-16 07:59:11 -0700

    converted to p5.js and removed simulation class

--- src/cards/Options.js
+++ src/cards/Options.js
@@ -5,15 +5,13 @@ import Option from './Option.js'
 
 export default function Options(props) {
   const optionRanges = props.data.optionRanges;
-  const updateFromOptions = props.data.updateFromOptions;
 
   return (
     <div id="Options">
       {optionRanges(props.state).map((option) => 
         {return <Option
                   key={option.name} 
-                  update={updateFromOptions(option)} 
-                  option={option}
+                  optionRanges={props.optionRanges}
                  />}
       )}
     </div>
