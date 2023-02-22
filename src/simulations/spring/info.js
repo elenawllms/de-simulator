d1a3646d6ca2306c8c29077702ddfef3e6dfdd4a
Elena Williams <elena.sivanjali.williams@gmail.com>
2023-02-21 22:39:10 -0800

    updated info tabs

--- src/simulations/spring/info.js
+++ src/simulations/spring/info.js
@@ -1,12 +1,17 @@
 var Latex = require('react-latex');
 
+const equation = `$$ m\\ddot{x} + b\\dot{x} + kx = 0 $$`
+
 const info = <>
 <h1>Spring</h1>
-This simulation models a pendulum, allowing for control of initial conditions, pendulum length, and damping.
+This simulation models a pendulum, allowing for control of initial conditions, pendulum length, and damping. Importantly, this simulation does not include gravity; the vertical layout of the spring is slightly misleading but makes it easier to see the spring and the options at the same time.
+<br/><br/>
 The pendulum obeys the formula
 <Latex displayMode={true}>
-    $ \theta '' + c\theta ' + \lambda\theta = F_0 \cos \Omega t $
+    {equation}
 </Latex>
+where <Latex>$m$</Latex> is the mass of the weight on the spring, <Latex>$b$</Latex> is the damping coefficient, <Latex>$k$</Latex> is the spring constant, and <Latex>$x$</Latex> is the displacement from equilibrium. This is derived from Hooke's Law with a damping term added.
+<br/><br/>
 We approximate the pendulum using 
 the <a href="https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods">Runge-Kutta</a> 4th order method.
 </>;
