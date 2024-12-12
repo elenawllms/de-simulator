d092161ec67894243e16f0b072ba600926fb5f88
Elena Williams <elena.sivanjali.williams@gmail.com>
2024-12-11 16:01:09 -0800

    Update FrontPageCard.js

--- src/templates/FrontPageCard.js
+++ src/templates/FrontPageCard.js
@@ -6,7 +6,7 @@ export default function FrontPageCard(props) {
     return (
         <Link to={props.link}>
             <div className='card'>
-                {/* <img src={require(`../simulations/${props.simulation}/cover.png`)} alt=""/> */}
+                <img src={require(`../simulations/${props.simulation}/cover.png`)} alt=""/>
                 <h2 className='title'>{props.title}</h2>
                 <p className="desc">{props.desc}</p>
             </div>
