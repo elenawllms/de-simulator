d321b6554c622173e1a0cac4efecf52c58749f5d
Elena Williams <elena.sivanjali.williams@gmail.com>
2024-12-11 16:00:36 -0800

    Remove paragraph description

--- src/Main.js
+++ src/Main.js
@@ -4,9 +4,9 @@ import FrontPageCard from './templates/FrontPageCard';
 export default function Main() {
   return (
     <div id="Main">
-      <h1>Simulations of Differential Equations</h1>
+      <h1>DE Simulations Homepage</h1>
       <h2>HMC Math 82: Differential Equations</h2>
-      <p>This is a new tool to interactively simulate differential equations. It is still in development, though, so I apologize for any buggy behavior! In particular, <strong>do not view this website on a mobile phone</strong>. Ideally, view it in the browser of your computer; a tablet works too, though it might be slower with the 3D Energy Diagram tabs.</p>
+      <p></p>
       <div className='cardLinks'>
         <FrontPageCard link="/pendulum" title="Pendulum" desc="A pendulum with variable length, damping, forcing, and initial angle/velocity."/>
         <FrontPageCard link="/brusselator" title="Brusselator" desc="A version of the Belousov-Zhabotinsky reaction, an oscillating chemical system. The system either approaches a steady-state point or a limit cycle, depending on system parameters."/>
